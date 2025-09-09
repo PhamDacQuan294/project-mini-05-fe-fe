0f5e9ce2b79b5ab00cf2d279f078b92d93891656
quan <quan93909@gmail.com>
2025-09-09 12:28:33 +0700

    Lấy danh sách topic

--- src/pages/Topic/index.js
+++ src/pages/Topic/index.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getListTopic } from "../../services/topicService";
+
 function Topic() {
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await getListTopic();
+      setTopics(response);
+    }
+    fetchApi();
+  }, []);
+
   return (
     <>
-      Topic
+      <h2>Danh sach chu de</h2>
+
+      {topics.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Ten chu de</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {topics.map(item => (
+              <tr key={item._id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>
+                  <Link to={"/quiz/" + item.id}>Lam bai</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   )
 }
