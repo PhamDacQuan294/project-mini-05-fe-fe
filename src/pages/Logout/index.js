f65f4f6902470329da1a981caf027a4be66dd656
quan <quan93909@gmail.com>
2025-09-09 11:03:57 +0700

    Làm đăng xuất

--- src/pages/Logout/index.js
+++ src/pages/Logout/index.js
@@ -1,7 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useNavigate } from "react-router-dom";
+import { deleteAllCookies } from "../../helpers/cookie";
+import { useEffect } from "react";
+
 function Logout() {
+  const navigate = useNavigate();
+
+  deleteAllCookies();
+
+  useEffect(() => {
+    navigate("/login");
+  }, []);
+
   return (
     <>
-      Logout
+      
     </>
   )
 }
