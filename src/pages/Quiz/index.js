351c5f3a25acc19f0a0e8d61c3cc3ab72cd95281
quan <quan93909@gmail.com>
2025-09-10 07:45:15 +0700

    Trang kết quả
    sau khi luyện tập xong sẽ hiển thị kết quả và số câu trả lời đúng, sai, % đúng

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -33,7 +33,7 @@ function Quiz() {
 
     let selectedAnswers = [];
 
-    for(let i = 0; i < e.target.elements.length; i++) { // e.target.elements.length nhung o input (dau tron)
+    for(let i = 0; i < e.target.elements.length; i++) { 
       if(e.target.elements[i].checked) {
         const name = e.target.elements[i].name;
         const value = e.target.elements[i].value;
@@ -52,8 +52,9 @@ function Quiz() {
     };
 
     const response = await createAnswer(options);
+
     if(response) {
-      navigate(`/result/${response.id}`);
+      navigate(`/result/${response.data._id}`);
     }
   }
 
@@ -85,25 +86,3 @@ function Quiz() {
 
 export default Quiz;
 
-
-/*
-  Ta khong the nao cho {item.id} chỗ câu được vì có nhiều bài quiz id sẽ bắt đầu từ 40, 41 nếu
-  mà item.id thế thì bắt đầu từ 40 à, nên là index.
-
-  <input type="radio" name="" value="" id="" /> chỗ này thử để name rỗng, rồi để name là test xem thử nó như thế nào. 
-  nếu để name rỗng thì nó sẽ tích hết, còn để name test thì nó sẽ chỉ tích được 1 câu, tích câu tiếp theo nó sẽ mất,
-  name={item.id} đặt như này thì có nghĩa là cho mỗi cái ô input của mỗi câu nhóm chung lại là 1 id.
-  nghĩa là cho cái ô input của câu 1 có id là 1, ô input câu 2 có id là 2.
-
-  cái id thì đặt tên chi cũng được nhưng mà phải là chữ cái đứng đằng trước.
-
-  value thì lưu cái kết quả của người chọn, ví dụ câu 1 người chọn chọn đáp án 2 thì lưu chỉ số là 1 để chập so sánh với cái 
-  "correctAnswer": 2 của bên questions.
-
-
-  <input type="radio" name={item.id} value={indexAns} id={`quiz-${item.id} /> nếu id của mình nối chỉ tới vậy thì trong một câu các ô tròn (trả lời)
-  sẽ có cùng id nên là mình phải nối thêm <input type="radio" name={item.id} value={indexAns} id={`quiz-${item.id}-${indexAns}`} /> thì mỗi ô input trong mỗi câu sẽ
-  không có cùng id, ví dụ câu 1 thì có 3 ô input thì 3 ô này đều có id khác nhau.
-
-  trong react for là htmlFor.
-*/
--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,7 +1,72 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getAnswer } from "../../services/answersService";
+import { getListQuestion } from "../../services/questionsService";
+import "./Result.css";
+
 function Result() {
+  const params = useParams();
+  const [dataResult, setDataResult] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const dataAnswers = await getAnswer(params.id);
+      const dataQuestions = await getListQuestion(dataAnswers[0].topicId);
+
+      let resultFinal = [];
+
+      for (let i = 0; i < dataQuestions.length; i++) {
+        resultFinal.push({
+          ...dataQuestions[i],
+          ...dataAnswers[0].answers.find(item => item.questionId === dataQuestions[i]._id) //
+        })
+      }
+
+      setDataResult(resultFinal);
+    }
+    fetchApi();
+  }, []);
+
+  
   return (
     <>
-      Result
+      <h1>Ket qua:</h1>
+      <div className="result__list">
+        {dataResult.map((item, index) => (
+          <div className="result__item" key={item._id}>
+            <p>
+              Cau {index + 1}: {item.question}
+
+              {item.correctAnswer === item.answer ? (
+                <span className="result__tag result__tag--true">Dung</span>
+              ) : (
+                <span className="result__tag result__tag--false">Sai</span>
+              )}
+            </p>
+            {item.answers.map((itemAns, indexAns) => {
+              let className = "";
+              let checked = false;
+
+              if(item.answer === indexAns) {
+                checked = true;
+                className = "result__item--selected";
+              }
+
+              if(item.correctAnswer === indexAns) {
+                className += " result__item--result";
+              }
+
+              return (
+                <div className="result__answer" key={indexAns}>
+                  <input type="radio" checked={checked} disabled/>
+                  <label className={className}>{itemAns}</label>
+                </div>
+              )
+            })}
+          </div>
+        ))}
+      </div>
     </>
   )
 }
